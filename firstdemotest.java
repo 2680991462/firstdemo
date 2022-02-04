11dc4242846112b3e14f2fc3e348b4bb9d08a33e
hys12138- <yuh045@ucsd.edu>
2022-02-03 22:20:58 -0800

    change

--- firstdemotest.java
+++ firstdemotest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class firstdemotest {
     @Test 
     public void testminus(){
-        assertEquals(1, firstdemo.minus(10, 5));
+        assertEquals(1, firstdemo.minus(10, 9));
     }
 }
